4263e0ec2e4b7cbeeec7ff7a81d46719e7b35899
Cássio Morais <cassiomoraisg@hotmail.com>
2022-01-07 11:55:05 -0300

    Add project files.

---
+++ src/Api/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
